871037e35ac5d834638303b30ac42b14f40d0b0e
RishiG1234 <67072625+RishiG1234@users.noreply.github.com>
2020-07-31 15:06:24 -0400

    Update trashcan.js

--- trashcan.js
+++ trashcan.js
@@ -1,48 +1,52 @@
-class Trashcan {
-    constructor(x,y) {
-        this.x=x;
-        this.y=y;
-        this.TrashcanWidth=200;
-        this.TrashcanHeight=213;
-        this.wallThickness=20;
-        this.image = loadImage("trashcan_image.png");
-        this.bottomBody=Bodies.rectangle(this.x, this.y, this.TrashcanWidth, this.wallThickness, 
-        {isStatic:true}) 
-        this.leftWallBody=Bodies.rectangle(this.x-this.TrashcanWidth/2, this.y-this.TrashcanHeight/2,
-        this.wallThickness, this.TrashcanHeight, {isStatic:true}) 
-        this.rightWallBody=Bodies.rectangle(this.x+this.TrashcanWidth/2, 
-        this.y-this.TrashcanHeight/2, this.wallThickness, this.TrashcanHeight, {isStatic:true}) 
-        World.add(world, this.bottomBody) 
-        World.add(world, this.leftWallBody) 
-        World.add(world, this.rightWallBody);
-
-    }
-    display() {
-        var posBottom=this.bottomBody.position;
-        var posLeft=this.leftWallBody.position;
-        var posRight=this.rightWallBody.position;
-        push();
-        translate(posLeft.x,posLeft.y);
-        rectMode(CENTER);
-        fill(255);
-        angleMode(RADIANS);
+class dustbin { 
+    constructor(x,y) { 
+        this.x=x; 
+        this.y=y; 
+        this.dustbinWidth=200; 
+        this.dustbinHeight=213; 
+        this.wallThickness=20; 
+        this.image=loadImage("trashcan_image.png");
+        this.bottomBody=Bodies.rectangle(this.x, this.y, this.dustbinWidth, this.wallThickness, {isStatic:true}); 
+        this.leftWallBody=Bodies.rectangle(this.x-this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true}); 
+        this.rightWallBody=Bodies.rectangle(this.x+this.dustbinWidth/2, this.y-this.dustbinHeight/2, this.wallThickness, this.dustbinHeight, {isStatic:true}); 
+        World.add(world, this.bottomBody); 
+        World.add(world, this.leftWallBody); 
+        World.add(world, this.rightWallBody); 
+    } 
+    display() { 
+        var posBottom=this.bottomBody.position; 
+        var posLeft=this.leftWallBody.position; 
+        var posRight=this.rightWallBody.position; 
+        push(); 
+        translate(posLeft.x, posLeft.y); 
+        rectMode(CENTER); 
+        //strokeWeight(4); 
+        angleMode(RADIANS); 
+        fill(255); 
         rotate(this.angle);
-        pop();
-        push();
-        translate(posRight.x,posRight.y);
-        rectMode(CENTER);
-        fill(255);
-        angleMode(RADIANS);
-        rotate(-1*this.angle);
-        pop();
-        push();
-        translate(posBottom.x,posBottom.y+10);
-        rectMode(CENTER);
-        fill(255);
-        angleMode(RADIANS);
-        rotate(this.angle);
-        imageMode(CENTER);
-        image(this.image,0,0,-this.TrashcanHeight/2,this.TrashcanWidth,this.TrashcanHeight);
-        pop();
-    }
+        //rect(0,0,this.wallThickness, this.dustbinHeight); 
+        pop(); 
+        push(); 
+        translate(posRight.x, posRight.y); 
+        rectMode(CENTER); 
+        //strokeWeight(4); 
+        angleMode(RADIANS); 
+        fill(255); 
+        rotate(-1*this.angle); 
+        //rect(0,0,this.wallThickness, this.dustbinHeight); 
+        pop(); 
+        push(); 
+        translate(posBottom.x, posBottom.y+10); 
+        rectMode(CENTER); 
+        //strokeWeight(4); 
+        angleMode(RADIANS); 
+        fill(255); 
+        imageMode(CENTER); 
+        image(this.image, 0,-this.dustbinHeight/2,this.dustbinWidth, this.dustbinHeight); 
+        //rect(0,0,this.dustbinWidth, this.wallThickness); 
+        pop(); 
+    } 
 }
+
+
+    
